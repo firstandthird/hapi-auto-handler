36ddc0ca5b65b5a08603016320001045d9d79967
Chris <orthagonal1@gmail.com>
2016-10-07 13:40:30 -0500

    first working version

---
+++ index.js
@@ -0,0 +1,41 @@
+'use strict';
+const async = require('async');
+const defaultMethod = require('lodash.defaults');
+const unset = require('lodash.unset');
+const defaults = {};
+
+exports.register = (server, options, next) => {
+  options = defaultMethod(options, defaults);
+  // define an 'auto' handler that routes can use:
+  server.handler('auto', (route, autoOptions) => {
+    return (request, reply) => {
+      // a copy of the server is available within the auto methods as results.server:
+      autoOptions.server = (done) => {
+        done(null, server);
+      };
+      // a copy of the request is avilabable within the auto methods as results.request:
+      autoOptions.request = (done) => {
+        done(null, request);
+      };
+      // run the async.auto expression:
+      async.auto(autoOptions, (err, results) => {
+        if (err) {
+          server.log(['error', 'hapi-auto-handler'], err);
+          return reply(err.toString()).code(500);
+        }
+        if (autoOptions.reply) {
+          return reply(results.reply);
+        }
+        // must unset these before hapi can return the results object:
+        unset(results, 'server');
+        unset(results, 'request');
+        reply(results);
+      });
+    };
+  });
+  next();
+};
+
+exports.register.attributes = {
+  pkg: require('./package.json')
+};
